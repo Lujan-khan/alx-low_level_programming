46befb111e362f242c8eb30c9e5926a903da0f2b
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-07-01 02:18:57 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,9 @@
 /**
  * main - run the main function
  *
- * runs printf and print statement
- * Return set to zero
+ * Description: Runs printf and print statement
+ * Return: Return 0 to exit
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
