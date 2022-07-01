9c668f0616b497410832b52e44ab8ba6817b1561
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-07-01 02:22:57 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,6 @@
  */
 
 int main(void)
-
 {
 	/* sizes of various data types*/
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));     
@@ -16,6 +15,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	
 	return (0);
 }	
