e443e886556173b0c4705416eb7454cde45bd6c1
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-07-01 02:30:19 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
 
+
+/**
+ * main - run all functions
+ *
+ * Description: Change standard error message
+ * Return: Return 1 and exit
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
