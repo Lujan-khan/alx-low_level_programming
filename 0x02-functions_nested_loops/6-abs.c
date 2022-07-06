4f9332482636dcb0738b72709706d2cc72bc28d8
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-07-06 21:30:18 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,20 @@
-dad
+/*
+ * File: 6-abs.c
+ * Auth: Fidelis Kanu
+ */
+
+#include "holberton.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
