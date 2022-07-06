1a4a55c4d32b1f610d37eb9c504f9004e8a02764
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-07-06 21:08:34 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,27 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
 #include "main.h"
 
 /**
- * main - Prints Holberton
- * @void: Empty parameter list for main.
+ * main - Prints "Holberton" followed by a new line.
  *
- * Description: Prints Holberton w/out stdio
- *
- * Return: 0 for success
-*/
+ * Return: Always 0.
+ */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
