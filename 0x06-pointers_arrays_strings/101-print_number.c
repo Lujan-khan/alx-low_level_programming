09e127705b73f321fc459b260b1831d71e19b34e
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-07-14 21:20:27 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,60 +10,17 @@
  *
  **/
 
-int exponent(int x, int y)
+int main(void)
 {
-	int power;
-
-	power = x;
-
-	if (x == 0)
-		return (0);
-	if (y == 0)
-		return (1);
-
-	while (y >= 2)
-	{
-		power  = power * x;
-		y--;
-	}
-	return (power);
-}
-
-/**
- * print_number - print an int using only _putchar
- * @number: int to be printed by function
- *
- * Return: nothing
- **/
-
-void print_number(int number)
-{
-	int size, digit, counter, sign;
-
-	sign = 1;
-	digit = 0;
-	size = 1;
-	counter = number;
-
-	if (number < 0)
-	{
-		_putchar('-');
-		sign = -1;
-	}
-
-	for (; counter >= 10 || counter <= -10; size++)
-	{
-		counter = counter / 10;
-	}
-
-	counter = number;
-
-	while (size >= 2)
-	{
-		digit = (counter / exponent(10, size - 1)) * sign;
-		_putchar(digit + '0');
-		counter = counter % exponent(10, size - 1);
-		size--;
-	}
-	_putchar(sign * counter % 10 + '0');
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }
