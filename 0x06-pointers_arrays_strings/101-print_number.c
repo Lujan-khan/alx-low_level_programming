6818c59384904a4fe47951ef5ce753349b04e1a7
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-07-14 21:18:49 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,69 @@
-/*
- * File: 101-print_number.c
- * Auth: fidelis  kanu
- */
-
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
- */
-void print_number(int n)
-{
-	unsigned int num = n;
+ * exponent - x to the power of y
+ * @x: base number
+ * @y: exponent
+ * Description: calcuates x^y
+ * Return: x^y
+ *
+ **/
 
-	if (n < 0)
+int exponent(int x, int y)
+{
+	int power;
+
+	power = x;
+
+	if (x == 0)
+		return (0);
+	if (y == 0)
+		return (1);
+
+	while (y >= 2)
+	{
+		power  = power * x;
+		y--;
+	}
+	return (power);
+}
+
+/**
+ * print_number - print an int using only _putchar
+ * @number: int to be printed by function
+ *
+ * Return: nothing
+ **/
+
+void print_number(int number)
+{
+	int size, digit, counter, sign;
+
+	sign = 1;
+	digit = 0;
+	size = 1;
+	counter = number;
+
+	if (number < 0)
 	{
 		_putchar('-');
-		num = -num;
+		sign = -1;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	for (; counter >= 10 || counter <= -10; size++)
+	{
+		counter = counter / 10;
+	}
 
-	_putchar((num % 10) + '0');
+	counter = number;
+
+	while (size >= 2)
+	{
+		digit = (counter / exponent(10, size - 1)) * sign;
+		_putchar(digit + '0');
+		counter = counter % exponent(10, size - 1);
+		size--;
+	}
+	_putchar(sign * counter % 10 + '0');
 }
