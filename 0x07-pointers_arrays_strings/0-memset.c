8742d6b1d72883a492a82443259f1f7f72f527a6
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-07-18 22:19:12 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,52 +1,19 @@
-/*
- * File: 0-memset.c
- * Auth: kanu fidelis
- */
-
+#include <stdio.h>
 #include "main.h"
-
 /**
- * memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
- *
- * Return: A pointer to the filled memory area @s.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
+ **_memset -main entry.
+ *@s:char pointer
+ *@b: char variable
+ *@n: integer variable
+ *Description:function that fills memory with a constant byte.
+ * Return:char pointer
+ **/
+char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
-}
