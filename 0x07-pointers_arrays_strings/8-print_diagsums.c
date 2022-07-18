969c43efb95d3eb089182f52ea0b084595739e9c
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-07-19 00:12:46 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,12 +15,13 @@ sum1 = 0;
 sum2 = 0;
 
 for (x = 0; x < size; x++)
+{
+    sum1 += a[(size + 1) * x];
 
-sum1 += a[(size + 1) * x];
-
-for (y = 0; y < size; y++)
-
-sum2 += a[(size - 1) * (y + 1)];
-
+    for (y = 0; y < size; y++)
+    {
+        sum2 += a[(size - 1) * (y + 1)];
+     }
+}
 printf("%d, %d\n", sum1, sum2);
 }
