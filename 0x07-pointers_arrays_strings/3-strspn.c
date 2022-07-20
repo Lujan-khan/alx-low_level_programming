cae855c3854d783c70f1387b2dcb2b386b5b8d6c
Fidelis <kanuarinze@gmail.com>
2022-07-19 21:35:24 -0700

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,7 +2,7 @@
  *
  *  * File: 3-strspn.c
  *
- *   * Auth: Fidelis Kanu
+ *   * Auth: Brennan D Baraban
  *
  *    */
 
@@ -31,37 +31,28 @@
 unsigned int _strspn(char *s, char *accept)
 
 {
+	unsigned int bytes = 0;
+	int index;
 
-		unsigned int bytes = 0;
-
-		int index;
-
-
-
-		while (*s)
+	while(*s)
+	{
+		for (index = 0; accept[index]; index++)
 		{
-			for (index = 0; accept[index]; index++)
-
+			if (*s == accept[index])
 			{
-
-				if (*s == accept[index])
-
-				{
-
-					bytes++;
-
-					break;
-																						
-				}
-				else if (accept[index +1] == '\0')
-
-					return (bytes);
-
+				bytes++;
+				break;
 			}
-			s++;
+
+			else if (accept[index +1] == '\0')
+			{
+			
+		 		return (bytes);
+			}
 
 		}
+		s++;
+	}	
 
-
-		return (bytes);
-}
+	return (bytes);
+}	
