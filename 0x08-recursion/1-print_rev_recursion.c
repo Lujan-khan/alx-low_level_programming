7c993532a526cce47bbd88e7a5662d1d739237b7
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-07-21 02:08:45 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- *  _puts_recursion - print string
- *  @s: string to print
- *  Description: print string using recursion
- *  Return: nothing
- **/
-
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ *
+ * Return: void
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
@@ -15,5 +14,4 @@ void _print_rev_recursion(char *s)
 	}
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
-
 }
